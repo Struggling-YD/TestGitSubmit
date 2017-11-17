564adf52c8522b108cdec3c0e008a609f198824c
Struggling-YD <1572256055@qq.com>
2017-11-17 18:26:34 +0800

    添加按钮试试ing~

--- app/src/main/java/com/szaf/testgitsubmit/MainActivity.java
+++ app/src/main/java/com/szaf/testgitsubmit/MainActivity.java
@@ -2,12 +2,24 @@ package com.szaf.testgitsubmit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_toast = (Button) findViewById(R.id.btn_toast);
+        btn_toast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Toast", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
